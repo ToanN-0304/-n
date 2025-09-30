da98e733cf30d1a2da427db42ae5b4d4ee881740
ToanN-0304 <toan53075@gmail.com>
2025-10-01 06:55:59 +0700

    Add files via upload

---
+++ FILE CSDL SQL/CRE.sql
@@ -0,0 +1,84 @@
+﻿create database QLSV
+
+use QLSV 
+GO
+
+CREATE TABLE NGANH
+ (   MANGANH VARCHAR(20) PRIMARY KEY,
+     TENNGANH NVARCHAR(50) , 
+ )
+
+ CREATE TABLE LOP
+ ( MALOP VARCHAR(20) PRIMARY KEY,
+   TENLOP NVARCHAR(10),
+   MANGANH VARCHAR(20) FOREIGN KEY (MANGANH) REFERENCES NGANH(MANGANH),
+   KHOAHOC NVARCHAR(10),
+   HEDT NVARCHAR(50), 
+ )
+
+ CREATE TABLE SINHVIEN
+  (  MASV VARCHAR(20) PRIMARY KEY,
+     HOTEN NVARCHAR(60) ,
+	 MALOP VARCHAR(20) FOREIGN KEY (MALOP) REFERENCES LOP(MALOP),
+	 GT NVARCHAR(5) DEFAULT N'Nam',
+	 NGAYSINH DATE DEFAULT GETDATE(),
+	 DIACHI NVARCHAR(100),
+  )
+
+ Alter Table SINHVIEN
+ ADD MANGANH VARCHAR(20) FOREIGN KEY (MANGANH) REFERENCES NGANH(MANGANH)
+
+ INSERT INTO NGANH (MANGANH,TENNGANH)
+ VALUES (N'TH',N'Công nghệ thông tin'),
+        (N'CO',N'Công nghệ kỹ thuật ô tô'),
+		(N'TA',N'Ngôn ngữ anh')
+ 
+INSERT INTO LOP ([MALOP],[TENLOP],[MANGANH],[KHOAHOC])
+VALUES (001,N'TH29.34',N'TH',29),
+       (002,N'TH29.03',N'TH',29),
+	   (003,N'CO28.15',N'CO',28),
+       (004,N'TA29.21',N'TA',29),
+	   (005,N'TA29.05',N'TA',29),
+       (006,N'TH27.04',N'TH',27)
+
+INSERT INTO SINHVIEN ([MASV],[MALOP],[GT],[NGAYSINH],[DIACHI],[HOTEN])
+ VALUES (28231570,001,N'Nam','2005-04-03',N'Hà Nam',N'Nguyễn Bá Toàn'),
+        (29240001,001,N'Nam','2006-11-06',N'Hà Nội',N'Nguyễn Văn A'),
+        (29240002,001,N'Nam','2006-10-15',N'Thanh Hóa',N'Trần Văn B'),
+		(29240003,001,N'Nữ','2006-05-23',N'Hưng Yên',N'Nguyễn Thị H'),
+        (29240004,001,N'Nam','2006-09-06',N'Hà Nội',N'Bùi Quốc A'),
+
+        (29240011,002,N'Nam','2006-11-02',N'Hà Nội',N'Đào Quang H'),
+        (29240012,002,N'Nam','2006-10-03',N'Hà Nam',N'Trần Văn L'),
+		(29240013,002,N'Nữ','2006-04-23',N'Hưng Yên',N'Bùi Thị A'),
+        (29240014,002,N'Nam','2006-09-16',N'Hà Nội',N'Bùi Xuân H'),
+		(29240015,002,N'Nữ','2006-05-12',N'Hưng Yên',N'Phạm Thanh H'),
+
+        (28230001,003,N'Nam','2005-09-13',N'Bắc Ninh',N'Nguyễn Bá T'),
+        (28230002,003,N'Nam','2005-05-06',N'Bắc Giang',N'Trần Quang X'),
+		(28230003,003,N'Nam','2005-11-23',N'Tuyên Quang',N'Nguyễn Đình T'),
+        (28230004,003,N'Nam','2005-05-05',N'Hà Nam',N'Bùi Văn C'),
+        (28230005,003,N'Nam','2005-12-03',N'Hà Nội',N'Phạm Văn K'),
+
+		(29240111,004,N'Nam','2006-08-12',N'Bắc Giang',N'Đào Quang X'),
+        (29240112,004,N'Nam','2006-11-23',N'Hà Nam',N'Trần Văn D'),
+		(29240113,004,N'Nữ','2006-08-04',N'Hưng Yên',N'Bùi Thị B'),
+        (29240114,004,N'Nam','2006-07-16',N'Hà Nội',N'Bùi Xuân N'),
+		(29240115,004,N'Nữ','2006-02-12',N'Bắc Ninh',N'Phạm Thanh M'),
+
+		(29241111,005,N'Nam','2006-03-22',N'Bắc Giang',N'Vũ Xuân T'),
+        (29241112,005,N'Nam','2006-10-20',N'Hà Nam',N'Trần Văn V'),
+		(29241113,005,N'Nữ','2006-01-14',N'Hưng Yên',N'Bùi Thanh H'),
+        (29241114,005,N'Nam','2006-08-26',N'Hà Nội',N'Trần Xuân N'),
+		(29241115,005,N'Nữ','2006-02-15',N'Bắc Ninh',N'Phạm Kiều N'),
+
+		(27220001,006,N'Nam','2004-06-07',N'Hà Nam',N'Nguyễn Bá C'),
+		(27220002,006,N'Nam','2004-03-17',N'Hà Nam',N'Nguyễn Trọng P'),
+		(27220003,006,N'Nam','2004-11-08',N'Bắc Ninh',N'Trần Văn Q'),
+		(27220004,006,N'Nam','2004-04-04',N'Hà Nội',N'Lý Sự C'),
+		(27220005,006,N'Nam','2004-05-25',N'Hà Nội',N'Nguyễn Thị O')
+        
+UPDATE SINHVIEN
+SET MANGANH = N'TH'
+WHERE MALOP = 001
+
---
+++ FILE VB.NET/QLSVV/QLSV.Designer.vb
@@ -0,0 +1,268 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.dgSV = New System.Windows.Forms.DataGridView()
+        Me.btxoa = New System.Windows.Forms.Button()
+        Me.btsua = New System.Windows.Forms.Button()
+        Me.btthem = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.gbthuhep = New System.Windows.Forms.GroupBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.cboKHOA = New System.Windows.Forms.ComboBox()
+        Me.cboNGANH = New System.Windows.Forms.ComboBox()
+        Me.gbtim = New System.Windows.Forms.GroupBox()
+        Me.txtTIM = New System.Windows.Forms.TextBox()
+        Me.cboTIM = New System.Windows.Forms.ComboBox()
+        Me.Gbcn = New System.Windows.Forms.GroupBox()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        CType(Me.dgSV, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.gbthuhep.SuspendLayout()
+        Me.gbtim.SuspendLayout()
+        Me.Gbcn.SuspendLayout()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'dgSV
+        '
+        Me.dgSV.AllowUserToAddRows = False
+        Me.dgSV.AllowUserToDeleteRows = False
+        Me.dgSV.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.dgSV.Location = New System.Drawing.Point(292, 238)
+        Me.dgSV.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.dgSV.MultiSelect = False
+        Me.dgSV.Name = "dgSV"
+        Me.dgSV.ReadOnly = True
+        Me.dgSV.RowHeadersWidth = 51
+        Me.dgSV.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.dgSV.Size = New System.Drawing.Size(818, 302)
+        Me.dgSV.TabIndex = 0
+        '
+        'btxoa
+        '
+        Me.btxoa.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btxoa.Location = New System.Drawing.Point(9, 101)
+        Me.btxoa.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btxoa.Name = "btxoa"
+        Me.btxoa.Size = New System.Drawing.Size(125, 35)
+        Me.btxoa.TabIndex = 3
+        Me.btxoa.Text = "Xóa"
+        Me.btxoa.UseVisualStyleBackColor = True
+        '
+        'btsua
+        '
+        Me.btsua.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.btsua.Location = New System.Drawing.Point(9, 61)
+        Me.btsua.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btsua.Name = "btsua"
+        Me.btsua.Size = New System.Drawing.Size(125, 35)
+        Me.btsua.TabIndex = 4
+        Me.btsua.Text = "Sửa"
+        Me.btsua.UseVisualStyleBackColor = True
+        '
+        'btthem
+        '
+        Me.btthem.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.btthem.Location = New System.Drawing.Point(9, 21)
+        Me.btthem.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.btthem.Name = "btthem"
+        Me.btthem.Size = New System.Drawing.Size(125, 35)
+        Me.btthem.TabIndex = 5
+        Me.btthem.Text = "Thêm"
+        Me.btthem.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.BackColor = System.Drawing.SystemColors.Control
+        Me.Label1.Font = New System.Drawing.Font("Segoe UI Black", 12.0!)
+        Me.Label1.ForeColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label1.Location = New System.Drawing.Point(292, 7)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(278, 28)
+        Me.Label1.TabIndex = 7
+        Me.Label1.Text = "Hệ thống quản lý sinh viên"
+        '
+        'Label2
+        '
+        Me.Label2.BackColor = System.Drawing.Color.FromArgb(CType(CType(0, Byte), Integer), CType(CType(192, Byte), Integer), CType(CType(0, Byte), Integer))
+        Me.Label2.Location = New System.Drawing.Point(292, 37)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(828, 8)
+        Me.Label2.TabIndex = 8
+        '
+        'gbthuhep
+        '
+        Me.gbthuhep.Controls.Add(Me.Label4)
+        Me.gbthuhep.Controls.Add(Me.Label3)
+        Me.gbthuhep.Controls.Add(Me.cboKHOA)
+        Me.gbthuhep.Controls.Add(Me.cboNGANH)
+        Me.gbthuhep.Location = New System.Drawing.Point(654, 64)
+        Me.gbthuhep.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.gbthuhep.Name = "gbthuhep"
+        Me.gbthuhep.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.gbthuhep.Size = New System.Drawing.Size(330, 154)
+        Me.gbthuhep.TabIndex = 9
+        Me.gbthuhep.TabStop = False
+        Me.gbthuhep.Text = "Thu hẹp theo:"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Label4.Location = New System.Drawing.Point(21, 95)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(41, 16)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Khóa:"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.ForeColor = System.Drawing.SystemColors.Desktop
+        Me.Label3.Location = New System.Drawing.Point(21, 34)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 16)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Ngành:"
+        '
+        'cboKHOA
+        '
+        Me.cboKHOA.FormattingEnabled = True
+        Me.cboKHOA.Location = New System.Drawing.Point(21, 114)
+        Me.cboKHOA.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.cboKHOA.Name = "cboKHOA"
+        Me.cboKHOA.Size = New System.Drawing.Size(269, 24)
+        Me.cboKHOA.TabIndex = 1
+        '
+        'cboNGANH
+        '
+        Me.cboNGANH.FormattingEnabled = True
+        Me.cboNGANH.Location = New System.Drawing.Point(21, 53)
+        Me.cboNGANH.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.cboNGANH.Name = "cboNGANH"
+        Me.cboNGANH.Size = New System.Drawing.Size(269, 24)
+        Me.cboNGANH.TabIndex = 0
+        '
+        'gbtim
+        '
+        Me.gbtim.Controls.Add(Me.txtTIM)
+        Me.gbtim.Controls.Add(Me.cboTIM)
+        Me.gbtim.Location = New System.Drawing.Point(304, 63)
+        Me.gbtim.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.gbtim.Name = "gbtim"
+        Me.gbtim.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.gbtim.Size = New System.Drawing.Size(303, 155)
+        Me.gbtim.TabIndex = 10
+        Me.gbtim.TabStop = False
+        Me.gbtim.Text = "Tìm kiếm"
+        '
+        'txtTIM
+        '
+        Me.txtTIM.Location = New System.Drawing.Point(19, 82)
+        Me.txtTIM.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.txtTIM.Name = "txtTIM"
+        Me.txtTIM.Size = New System.Drawing.Size(226, 22)
+        Me.txtTIM.TabIndex = 1
+        '
+        'cboTIM
+        '
+        Me.cboTIM.FormattingEnabled = True
+        Me.cboTIM.Location = New System.Drawing.Point(19, 45)
+        Me.cboTIM.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.cboTIM.Name = "cboTIM"
+        Me.cboTIM.Size = New System.Drawing.Size(151, 24)
+        Me.cboTIM.TabIndex = 0
+        '
+        'Gbcn
+        '
+        Me.Gbcn.Controls.Add(Me.btthem)
+        Me.Gbcn.Controls.Add(Me.btsua)
+        Me.Gbcn.Controls.Add(Me.btxoa)
+        Me.Gbcn.Location = New System.Drawing.Point(12, 146)
+        Me.Gbcn.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Gbcn.Name = "Gbcn"
+        Me.Gbcn.Padding = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Gbcn.Size = New System.Drawing.Size(196, 146)
+        Me.Gbcn.TabIndex = 11
+        Me.Gbcn.TabStop = False
+        Me.Gbcn.Text = "Chức năng"
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 7)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(274, 134)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom
+        Me.PictureBox1.TabIndex = 12
+        Me.PictureBox1.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 16.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1122, 550)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Controls.Add(Me.Gbcn)
+        Me.Controls.Add(Me.gbtim)
+        Me.Controls.Add(Me.gbthuhep)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.dgSV)
+        Me.ForeColor = System.Drawing.SystemColors.Highlight
+        Me.Margin = New System.Windows.Forms.Padding(3, 2, 3, 2)
+        Me.Name = "Form1"
+        Me.Text = "Quản lý sinh viên"
+        CType(Me.dgSV, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.gbthuhep.ResumeLayout(False)
+        Me.gbthuhep.PerformLayout()
+        Me.gbtim.ResumeLayout(False)
+        Me.gbtim.PerformLayout()
+        Me.Gbcn.ResumeLayout(False)
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents dgSV As DataGridView
+    Friend WithEvents btxoa As Button
+    Friend WithEvents btsua As Button
+    Friend WithEvents btthem As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents gbthuhep As GroupBox
+    Friend WithEvents gbtim As GroupBox
+    Friend WithEvents Gbcn As GroupBox
+    Friend WithEvents cboKHOA As ComboBox
+    Friend WithEvents cboNGANH As ComboBox
+    Friend WithEvents Label3 As Label
+    Friend WithEvents cboTIM As ComboBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents txtTIM As TextBox
+    Friend WithEvents PictureBox1 As PictureBox
+End Class
