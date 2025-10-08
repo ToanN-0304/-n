08749e60bc1c3744acadcde71803525761bad963
Tòn N <toan53075@gmail.com>
2025-10-09 01:24:45 +0700

    Add files via upload

---
+++ FILE VB.NET/QLSVV/RoundedTextBox.vb
@@ -0,0 +1,93 @@
+﻿Imports System.Drawing
+Imports System.Drawing.Drawing2D
+Imports System.Windows.Forms
+
+Public Class RoundedTextBox
+    Inherits TextBox
+
+    Private _placeholderText As String = "Nhập thông tin..."
+    Private _isFocused As Boolean = False
+
+    '--- Thuộc tính Placeholder ---
+    Public Property PlaceholderText As String
+        Get
+            Return _placeholderText
+        End Get
+        Set(value As String)
+            _placeholderText = value
+            Invalidate() ' Vẽ lại khi thay đổi
+        End Set
+    End Property
+
+    Public Sub New()
+        MyBase.New()
+        Me.BorderStyle = BorderStyle.None
+        Me.Font = New Font("Segoe UI", 10)
+        Me.BackColor = Color.White
+        Me.ForeColor = Color.Black
+        Me.Padding = New Padding(5)
+        Me.SetStyle(ControlStyles.UserPaint, True)
+    End Sub
+
+    Protected Overrides Sub OnCreateControl()
+        MyBase.OnCreateControl()
+        Me.AutoSize = False
+
+    End Sub
+
+    Protected Overrides Sub OnEnter(e As EventArgs)
+        MyBase.OnEnter(e)
+        _isFocused = True
+        Invalidate()
+    End Sub
+
+    Protected Overrides Sub OnLeave(e As EventArgs)
+        MyBase.OnLeave(e)
+        _isFocused = False
+        Invalidate()
+    End Sub
+
+    Protected Overrides Sub OnPaint(e As PaintEventArgs)
+        Dim g As Graphics = e.Graphics
+        g.SmoothingMode = SmoothingMode.AntiAlias
+
+        ' Viền bo góc
+        Dim rect As New Rectangle(0, 0, Me.Width - 1, Me.Height - 1)
+        Dim path As New GraphicsPath()
+        Dim radius As Integer = 10
+
+        path.AddArc(rect.X, rect.Y, radius, radius, 180, 90)
+        path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90)
+        path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90)
+        path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90)
+        path.CloseFigure()
+
+        ' Nền
+        Using brush As New SolidBrush(Me.BackColor)
+            g.FillPath(brush, path)
+        End Using
+
+        ' Viền
+        Dim borderColor As Color = If(_isFocused, Color.DodgerBlue, Color.Silver)
+        Using pen As New Pen(borderColor, 2)
+            g.DrawPath(pen, path)
+        End Using
+
+        ' Text hoặc Placeholder
+        Dim textRect As RectangleF = New RectangleF(8, 8, Me.Width - 16, Me.Height - 16)
+        If Me.Text.Length > 0 Then
+            Using brush As New SolidBrush(Me.ForeColor)
+                g.DrawString(Me.Text, Me.Font, brush, textRect)
+            End Using
+        ElseIf Not _isFocused Then
+            Using brush As New SolidBrush(Color.Gray)
+                g.DrawString(_placeholderText, Me.Font, brush, textRect)
+            End Using
+        End If
+    End Sub
+
+    Protected Overrides Sub OnTextChanged(e As EventArgs)
+        MyBase.OnTextChanged(e)
+        Invalidate()
+    End Sub
+End Class
---
+++ FILE VB.NET/QLSVV/cre.vb
@@ -0,0 +1,73 @@
+﻿Imports MS.Internal
+
+Public Class fCRE
+    Private _DBAccess As New DataBaseAccess
+    Private Sub GroupBox1_Paint(sender As Object, e As PaintEventArgs) Handles GroupBox1.Paint
+        Dim gb As GroupBox = CType(sender, GroupBox)
+        Dim g As Graphics = e.Graphics
+        g.SmoothingMode = Drawing2D.SmoothingMode.AntiAlias
+        Dim rect As New Rectangle(0, 7, gb.Width - 1, gb.Height - 10)
+        Dim radius As Integer = 15
+        Dim path As New Drawing2D.GraphicsPath()
+        path.AddArc(rect.X, rect.Y, radius, radius, 180, 90)
+        path.AddArc(rect.Right - radius, rect.Y, radius, radius, 270, 90)
+        path.AddArc(rect.Right - radius, rect.Bottom - radius, radius, radius, 0, 90)
+        path.AddArc(rect.X, rect.Bottom - radius, radius, radius, 90, 90)
+        path.CloseAllFigures()
+        g.Clear(Me.BackColor)
+        g.FillPath(New SolidBrush(gb.BackColor), path)
+        g.DrawPath(New Pen(Color.LightGray, 1), path)
+        g.DrawString(gb.Text, gb.Font, Brushes.Black, 15, 0)
+    End Sub
+
+    Private Function addTK() As Boolean
+        Try
+
+            Dim sqlQuery As String = "INSERT INTO LG (TEN, MAGV, SDT, TK, PASS) " &
+                                 "VALUES (N'{0}', N'{1}', N'{2}', N'{3}', N'{4}')"
+
+            sqlQuery = String.Format(sqlQuery,
+                                 txtTEN.Text.Trim(),
+                                 txtM.Text.Trim(),
+                                 txtSDT.Text.Trim(),
+                                 txtTK.Text.Trim(),
+                                 txtMK.Text.Trim())
+
+            Return _DBAccess.ExecuteNoneQuery(sqlQuery)
+
+        Catch ex As Exception
+            MessageBox.Show("Lỗi SQL: " & ex.Message, "Error", MessageBoxButtons.OK, MessageBoxIcon.Error)
+            Return False
+        End Try
+    End Function
+
+    Private Function IsEmpty() As Boolean
+        Return (String.IsNullOrEmpty(txtTEN.Text) OrElse String.IsNullOrEmpty(txtM.Text) OrElse
+               String.IsNullOrEmpty(txtSDT.Text) OrElse String.IsNullOrEmpty(txtTK.Text) OrElse
+               String.IsNullOrEmpty(txtMK.Text))
+    End Function
+    Private Sub btCRE_Click(sender As Object, e As EventArgs) Handles btCRE.Click
+        If IsEmpty() Then
+            MessageBox.Show("Hãy nhập đủ dữ liệu", "Error", MessageBoxButtons.OK)
+        Else
+            If addTK() Then
+                MessageBox.Show("Thêm thành công!", "Thông báo", MessageBoxButtons.OK)
+                Me.DialogResult = Windows.Forms.DialogResult.OK
+                Dim lg As New fLG()
+                lg.Show()
+                Me.Close()
+            Else
+                MessageBox.Show("Thêm thất bại!", "Lỗi", MessageBoxButtons.OK)
+                End If
+            End If
+
+
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim lg As New fLG()
+        lg.Show()
+        Me.Close()
+
+    End Sub
+End Class
